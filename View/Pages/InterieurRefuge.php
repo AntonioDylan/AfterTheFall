5a2b6ab6c6408d2d153768c3fab388c71daf9f19
Clement <clement.mallejac.83@gmail.com>
2019-06-05 11:47:40 +0200

    Ajout Pages

---
+++ View/Pages/InterieurRefuge.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="fr" class="InterieurRefuge">
+
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel='stylesheet' href='../css/style.css' type='text/css'>
+    <link rel='stylesheet' href='../css/posBtn.css' type='text/css'>
+    <link rel='stylesheet' href='../fa/css/all.min.css' type='text/css'>
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/event.js"></script>
+    <!-- <script src="js/jsonReader.js"></script>
+     <script src="texte/scenario.js"></script> -->
+</head>
+
+<body>
+<div>
+    <div class="titreDescritpion">Intérieur d’un refuge</div>
+    <div class="containerTexteDescription">
+        <div class="texteDescription">
+            En dépit de leurs aspects sale et délabré, les refuges furent de vrais havres de paix pour les voyageurs fatigués qui y passaient. Ils leurs permettaient de se reposer après de longues journées de marche vers les sanctuaires.        </div>
+    </div>
+</div>
+<!--<div id="btn_VilleDepart-PopUp1" class="btnClick"> </div>-->
+<div class="blockNarrateur">
+    <div class="blockTextNarrateur"></div>
+    <div class="textNarrateur"></div>
+    <i class="far fa-2x fa-window-close btn closeTextNarrateur"></i>
+</div>
+<div class="fleches">
+    <div class="backFlecheHorizontal"></div>
+    <div class="backFlecheVerticalHaut"></div>
+    <div class="backFlecheVerticalBas"></div>
+    <a href="RefugeSud.php"><i class="fas fa-4x fa-arrow-down flecheBas"></i></a>
+    <a ><i class="fas fa-4x fa-arrow-up flecheHaut flecheDesactive"></i></a>
+    <a ><i class="fas fa-4x fa-arrow-left flecheGauche flecheDesactive"></i></a>
+    <a ><i class="fas fa-4x fa-arrow-right flecheDroite flecheDesactive"></i></a>
+</div>
+
+</body>
+
+</html>
---
+++ View/Pages/SouterrainSud.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr" class="SouterrainSud">
+
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel='stylesheet' href='../css/style.css' type='text/css'>
+    <link rel='stylesheet' href='../css/posBtn.css' type='text/css'>
+    <link rel='stylesheet' href='../fa/css/all.min.css' type='text/css'>
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/event.js"></script>
+    <!-- <script src="js/jsonReader.js"></script>
+     <script src="texte/scenario.js"></script> -->
+</head>
+
+<body>
+<div>
+    <div class="titreDescritpion">Souterrain Sud</div>
+    <div class="containerTexteDescription">
+        <div class="texteDescription">
+            Dans les premiers temps afin d’échapper à ce nouvel environnement en constante évolution, les voyageurs se réfugiaient sous terre. Nombre de galeries furent creusées pour se déplacer sans danger sous les ruines des refuges. Sortant des bunkers ces chemins souterrains ont pris plusieurs années avant d’être opérationnels, mais les voyageurs se sentaient plus en sécurité.    </div>
+    <!--<div id="btn_VilleDepart-PopUp1" class="btnClick"> </div>-->
+    <div class="blockNarrateur">
+        <div class="blockTextNarrateur"></div>
+        <div class="textNarrateur"></div>
+        <i class="far fa-2x fa-window-close btn closeTextNarrateur"></i>
+    </div>
+    <div class="fleches">
+        <div class="backFlecheHorizontal"></div>
+        <div class="backFlecheVerticalHaut"></div>
+        <div class="backFlecheVerticalBas"></div>
+        <a href="Bunker.php"><i class="fas fa-4x fa-arrow-right flecheDroite"></i></a>
+        <a href="CentreCommercial.php"><i class="fas fa-4x fa-arrow-left flecheGauche"></i></a>
+        <a ><i class="fas fa-4x fa-arrow-up flecheHaut flecheDesactive"></i></a>
+        <a ><i class="fas fa-4x fa-arrow-down flecheBas flecheDesactive"></i></a>
+    </div>
+
+</body>
+
+</html>

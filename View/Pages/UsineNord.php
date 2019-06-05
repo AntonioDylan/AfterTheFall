67af7054a8666737c21d5012274bc183e8363498
Asato4 <mickael.pierron7@orange.fr>
2019-06-05 14:46:50 +0200

    envois erreur pour dylan

--- View/Pages/UsineNord.php
+++ View/Pages/UsineNord.php
@@ -23,7 +23,15 @@
       Les plus éminents scientifiques s’y étaient rassemblés afin de réfléchir ensemble à un moyen d’éviter le sort qui allait s'abattre sur eux.
       </div>
     </div>
+    <div id="btn_UsineNord-PopUp1" data-numpop="popUp1" data-texte="Situé en Asie centrale, la mer d’Aral est un lac d’eau salé au milieu d’espaces désertiques. Dans les années 1960, il était encore alimenté par de puissants fleuves: il formait le plus quatrième plus grand lac du monde, avec une superficie de 67 000km². Dans les années 2000, cette superficie fût divisée par deux dû au détournement des fleuves qui l’alimentaient. Ce détournement est avait pour but d’irriguer des cultures de champs. En conséquence, 28 espèces endémiques de poissons disparurent à cause de pesticides présents au fond du bassin. Contaminant également les populations alentours le taux de mortalité infantile est l’un des plus élevé au monde dans cette région." class="btnClick"> </div>
     <div class="fondOpaque close"></div>
+    <div class="blockNarrateur close">
+      <div class="blockTextNarrateur"></div>
+      <div class="textNarrateur"></div>
+      <i class="far fa-2x fa-window-close btn  closeTextNarrateur"></i>
+    </div>
+
+    <div id="videoUsineNord1" class="close popUp1 displayNone"><iframe width="560" height="315" src="https://www.youtube.com/embed/balCdCps4HE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
   </div>
   <!--<div id="btn_VilleDepart-PopUp1" class="btnClick"> </div>-->
   <div class="blockNarrateur">
